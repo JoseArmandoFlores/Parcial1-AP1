852bb3afa22698ef6365751dc3ef971b08f25c00
Jose Armando Flores Baldera <jose_flores3@ucne.edu.do>
2019-09-23 16:34:43 -0400

    Creando el proyecto

--- Parcial1-AP1/MainForm.cs
+++ Parcial1-AP1/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Parcial1_AP1
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
